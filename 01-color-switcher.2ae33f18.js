758fa32ba7d3d60e400c0e686336c123b6f1a8e4
Nikolay-Kovalov <106543342+Nikolay-Kovalov@users.noreply.github.com>
2023-08-25 09:45:43 +0000

    Deploying to gh-pages from @ Nikolay-Kovalov/Homework9@620da1879de951e61babbfc51b3331e9dfc38316 🚀

---
+++ 01-color-switcher.2ae33f18.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]")};console.log(t.startBtn)}();
+//# sourceMappingURL=01-color-switcher.2ae33f18.js.map
---
+++ 01-color-switcher.a09c01f3.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]")};console.log(t.startBtn);
+//# sourceMappingURL=01-color-switcher.a09c01f3.js.map
